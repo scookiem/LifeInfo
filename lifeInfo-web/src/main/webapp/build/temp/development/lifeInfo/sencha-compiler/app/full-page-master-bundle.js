34c57f25d330c69b72fdb77384db319d8dc823a4
scookiem <luijunnan444@163.com>
2017-03-07 15:49:09 +0800

    初始化

--- lifeInfo-web/src/main/webapp/build/temp/development/lifeInfo/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require E:\extjs\workspace\lifeInfo\app.js
--- lifeInfo-web/src/main/webapp/build/temp/production/liifeInfo/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('..\..\..\..\..\lifeInfo\sass\config.rb')
-cache_path = 'E:\extjs\workspace/build/.sass-cache'
