dd0b846a6dc0caec061f55c23458857fb75adc33
jiahang.lin@funi365.com <jiahang.lin@funi365.com>
2017-03-07 10:24:24 +0800

    创建项目

---
+++ lifeInfo-web/src/main/webapp/build/temp/production/lifeInfo/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\lifeInfo\sass\config.rb')
+cache_path = 'E:\extjs\workspace/build/.sass-cache'
---
+++ lifeInfo-web/src/main/webapp/build/temp/production/liifeInfo/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require E:\extjs\workspace\lifeInfo\app.js
